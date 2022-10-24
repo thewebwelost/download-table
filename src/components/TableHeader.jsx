c9a3e4a43abef09671687aafd05895b0537a2723
thewebwelost <thawebwelost@gmail.com>
2022-10-24 09:43:02 -0700

    table header added

--- src/components/TableHeader.jsx
+++ src/components/TableHeader.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
-function TableHeader() {
-  return <div>TableHeader</div>;
+function TableHeader({ isChecked, count }) {
+  return (
+    <header>
+      <label>
+        <input
+          type={'checkbox'}
+          onChange={(e) => e.preventDefault()}
+          checked={isChecked}
+        />
+        Select all
+      </label>
+
+      {count > 0 ? `Selected: ${count}` : 'None selected'}
+
+      <div>
+        <button>Download selected</button>
+      </div>
+    </header>
+  );
 }
 
 export default TableHeader;
